fd5a4760f0446aad926f333a313cd3d9fcc61c1d
Tristan De Luna <44929175+techpair@users.noreply.github.com>
2024-09-22 10:59:22 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,24 @@
+from transformers import Trainer, TrainingArguments
+
+# ... (load data, tokenizer, model)
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    overwrite_output_dir=True,
+    num_train_epochs=3,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    evaluation_strategy="steps",
+    eval_steps=100,
+    save_steps=100,
+    logging_steps=100,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+)
+
+trainer.train()
